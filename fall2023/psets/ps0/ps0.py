8628dd17522297375bee95ebc3f52a0f29255e75
Nalan123 <102498346+Nalan123@users.noreply.github.com>
2023-09-13 23:16:01 -0400

    Update ps0.py

--- fall2023/psets/ps0/ps0.py
+++ fall2023/psets/ps0/ps0.py
@@ -31,7 +31,18 @@ class BTvertex:
 # Runtime: O(n)
 def calculate_sizes(v):
     # Your code goes here
-    pass
+    if v is None: 
+        return 0
+    
+    left_subtreee_size = calculate_sizes(v.left)
+
+    right_subtree_size = calculate_sizes(v.right)
+
+    total_size  = left_subtreee_size + right_subtree_size + 1
+
+    v.size = total_size
+
+    return total_size
 
 #
 # Problem 1c
@@ -41,7 +52,39 @@ def calculate_sizes(v):
 # ... of size n and height h
 # Output: A BTvertex that, if removed from the tree, would result
 # ... in disjoint trees that all have at most n/2 vertices
-# Runtime: O(h)
+# Runtime: O(h
 def find_vertex(r): 
     # Your code goes here
-    pass
+    if not r:
+        return None
+    
+    def find_size(v):
+        if not v:
+            return None 
+
+
+        vertex = None 
+
+        if largest_subtree(v, r) <= r.size/2:
+            vertex = v 
+        elif v.left and largest_subtree(v.left, r) > r.size/2:
+            return find_size(v.left)
+        else:
+            return find_size(v.right)
+
+        return vertex
+
+    def largest_subtree(v,r):
+        max_size = float("-inf")
+        if v.left and not v.right:
+            max_size = max(v.left.size, r.size- v.size)
+        elif v.right and not v.left: 
+            max_size = max(v.right.size, r.size- v.size)
+        else:
+            max_size = r.size - v.size
+
+        return max_size
+
+    return find_size(r)
+
+
